bbf2138285277f81fd883df40411da22522c0684
Khamtsev <dhamcev@yandex.ru>
2024-08-14 17:42:49 +0300

    Fix nginx conf

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -5,21 +5,13 @@ from pathlib import Path
 
 load_dotenv()
 
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', default='secret_token')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', default='false').lower() in ('true', '1')
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '')
+ALLOWED_HOSTS = ['*']
 
 DOMAIN = os.getenv('DOMAIN', default='test.com')
 # Application definition
