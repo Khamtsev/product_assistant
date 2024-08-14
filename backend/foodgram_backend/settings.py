a75a7f40bf3632cc03ee1365c9876cb58a8d0782
Khamtsev <dhamcev@yandex.ru>
2024-08-14 19:27:22 +0300

    Autodeploy test

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -108,10 +108,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
