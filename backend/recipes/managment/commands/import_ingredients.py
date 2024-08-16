fbbbcab9b332ad66d2d85f88a8077f7a857550f9
Khamtsev <dhamcev@yandex.ru>
2024-08-16 14:41:42 +0300

    Add import commands

---
+++ backend/recipes/managment/commands/import_ingredients.py
@@ -0,0 +1,24 @@
+import json
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    help = 'Импортирование ингредиентов из ingredients.json'
+
+    def handle(self, *args, **options):
+        file_path = f'{settings.IMPORT_FOLDER}/ingredients.json'
+
+        with open(file_path, 'r', encoding='utf-8') as file:
+            ingredients = json.load(file)
+
+            Ingredient.objects.bulk_create(
+                Ingredient(**ingredient) for ingredient in ingredients
+            ),
+
+        self.stdout.write(self.style.SUCCESS(
+            'Ингредиенты успешно импортированы'
+        ))
---
+++ backend/recipes/managment/commands/import_tags.py
@@ -0,0 +1,24 @@
+import json
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from recipes.models import Tag
+
+
+class Command(BaseCommand):
+    help = 'Импортирование тегов из tags.json'
+
+    def handle(self, *args, **options):
+        file_path = f'{settings.IMPORT_FOLDER}/tags.json'
+
+        with open(file_path, 'r', encoding='utf-8') as file:
+            tags = json.load(file)
+
+            Tag.objects.bulk_create(
+                (Tag(**tag) for tag in tags),
+            )
+
+        self.stdout.write(self.style.SUCCESS(
+            'Теги успешно импортированы'
+        ))
