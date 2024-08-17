4828f2ad732573710ec8c70ac8dd147445d2691f
Khamtsev <dhamcev@yandex.ru>
2024-08-17 12:49:33 +0300

    Fix pagination

--- backend/api/views.py
+++ backend/api/views.py
@@ -9,7 +9,6 @@ from recipes.models import (Favorite, Follow, Ingredient, Recipe, ShoppingCart,
                             Tag)
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
